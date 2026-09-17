de22ab24f13e8bdebc453d06b72ad3a3e63c025b
shinobu_kage <maksim.lub@icloud.com>
2026-09-17 20:00:06 +0300

    last update

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,52 +1,12 @@
-import { useState } from "react";
-import SearchBar from "../SearchBar/SearchBar";
-import { fetchMovies } from "../../services/movieService";
-import type { Movie } from "../../types/movie";
-import toast, { Toaster } from "react-hot-toast";
-import MovieGrid from "../MovieGrid/MovieGrid";
-import MovieModal from "../MovieModal/MovieModal";
-import Loader from "../Loader/Loader";
-import ErrorMessage from "../ErrorMessage/ErrorMessage";
-export default function App() {
-  const [movies, setMovies] = useState<Movie[]>([]);
-  const [movie, setMovie] = useState<Movie | null>(null);
-  const [loader, setLoader] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(false);
-  const handleSubmit = async (topic: string) => {
-    try {
-      setLoader(true);
-      setMovies([]);
-      setErrorMessage(false);
-      const data = await fetchMovies(topic);
-      setMovies(data);
-      setLoader(false);
-      if (data.length === 0) {
-        toast.error("No movies found for your request.");
-      }
-    } catch {
-      toast.error("This didn't work.");
-      setErrorMessage(true);
-    } finally {
-      setLoader(false);
-    }
-  };
-  const handleSelect = (movie: Movie) => {
-    setMovie(movie);
-  };
 
-  const closeModal = () => {
-    setMovie(null);
-  };
+import OrderForm from '../OrderForm/OrderForm';
+
+export default function App() {
+  
+
   return (
     <>
-      <SearchBar onSubmit={handleSubmit} />
-      {movies.length > 0 && (
-        <MovieGrid onSelect={handleSelect} movies={movies} />
-      )}
-      {movie && <MovieModal movie={movie} onClose={closeModal} />}
-      {loader && <Loader />}
-      {errorMessage && <ErrorMessage />}
-      <Toaster />
+      <OrderForm/>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,16 @@
 // src/main.tsx
-
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 // Нормалізація стилів
 import "modern-normalize";
 // Глобальні стилі (додатково)
 import "./global.css";
-
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>,
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>
 );
